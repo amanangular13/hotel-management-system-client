833c54ae363ccc0187e85583d813dfd663075dd0
Aman Verma <aman.angular13@gmail.com>
2025-11-15 16:16:40 +0530

    added landing page

--- src/app/layout/main/main.ts
+++ src/app/layout/main/main.ts
@@ -1,40 +1,13 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
 export class Main {
-  http = inject(HttpClient);
   authService = inject(AuthService);
-
-  onClick() {
-    this.http
-      .post<any>(
-        'https://api-gateway-production-5174.up.railway.app/api/v1/auth/login',
-        {
-          email: 'aman@gmail.com',
-          password: 'amanverma1234',
-        },
-        { withCredentials: true }
-      )
-      .subscribe((res) => {
-        this.authService.setRole(res.data.role);
-      });
-  }
-
-  onUpdate() {
-    this.http
-      .get<any>(
-        'https://api-gateway-production-5174.up.railway.app/api/v1/users/email/aman@gmail.com',
-        { withCredentials: true }
-      )
-      .subscribe((res) => {
-        console.log(res.data);
-      });
-  }
 }
